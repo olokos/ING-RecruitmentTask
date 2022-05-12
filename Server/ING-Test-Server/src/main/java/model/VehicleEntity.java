f9672f89da33659c065e6039f58415957854f9d0
olokos <1755581+olokos@users.noreply.github.com>
2022-05-13 00:15:44 +0200

    Fix creating of tables with H2

--- Server/ING-Test-Server/src/main/java/model/VehicleEntity.java
+++ Server/ING-Test-Server/src/main/java/model/VehicleEntity.java
@@ -1,10 +1,12 @@
 package model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
-import java.util.Set;
 
 @Entity
 @Table(name = "vehicle")
--- Server/ING-Test-Server/src/main/java/pl/olokos/ingtestserver/IngTestServerApplication.java
+++ Server/ING-Test-Server/src/main/java/pl/olokos/ingtestserver/IngTestServerApplication.java
@@ -2,8 +2,10 @@ package pl.olokos.ingtestserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("model")
 public class IngTestServerApplication {
 
     public static void main(String[] args) {
