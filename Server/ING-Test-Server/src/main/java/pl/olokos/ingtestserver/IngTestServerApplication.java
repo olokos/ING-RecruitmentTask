f0fd071574534641f67874f3507b510c6c2f4e46
olokos <1755581+olokos@users.noreply.github.com>
2022-05-12 19:17:29 +0200

    Initial commit

---
+++ Server/ING-Test-Server/src/main/java/pl/olokos/ingtestserver/IngTestServerApplication.java
@@ -0,0 +1,13 @@
+package pl.olokos.ingtestserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngTestServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngTestServerApplication.class, args);
+    }
+
+}
---
+++ Server/ING-Test-Server/src/test/java/pl/olokos/ingtestserver/IngTestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.olokos.ingtestserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngTestServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
