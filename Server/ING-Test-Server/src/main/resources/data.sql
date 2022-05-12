34d5898d1f08e500f65adc09e956e7d424122296
olokos <1755581+olokos@users.noreply.github.com>
2022-05-13 00:34:26 +0200

    Fix input data and defer loading of initial data

--- Server/ING-Test-Server/src/main/resources/data.sql
+++ Server/ING-Test-Server/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO vehicle (vehicle_id, make, model, number_plate, purchased_for, vehicle_type)
-VALUES (1, 'Subaru', 'Impreza', 'WXG325', 50000 1),
-       (2, 'Audi', 'A3', 'WXB132', 25000 2),
-       (3, 'Toyota', 'Yaris GR', 'LU5532', 120000 3),
+VALUES (1, 'Subaru', 'Impreza', 'WXG325', 50000, 1),
+       (2, 'Audi', 'A3', 'WXB132', 25000, 2),
+       (3, 'Toyota', 'Yaris GR', 'LU5532', 120000, 3),
        (4, 'Aston Martin', 'Vantage V12', 'LU3232', 400000, 3),
        (5, 'Nissan', 'Fairlady Z', 'WA8922', 90000, 3),
        (6, 'Toyota', 'Supra', 'WF6546', 130000, 3),
