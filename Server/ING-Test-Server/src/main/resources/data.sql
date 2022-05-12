b2bff6b1b3fc144d744de791615d9caf90848ecf
olokos <1755581+olokos@users.noreply.github.com>
2022-05-13 00:37:41 +0200

    Make initial data more useful

--- Server/ING-Test-Server/src/main/resources/data.sql
+++ Server/ING-Test-Server/src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO vehicle (vehicle_id, make, model, number_plate, purchased_for, vehicle_type)
 VALUES (1, 'Subaru', 'Impreza', 'WXG325', 50000, 1),
-       (2, 'Audi', 'A3', 'WXB132', 25000, 2),
-       (3, 'Toyota', 'Yaris GR', 'LU5532', 120000, 3),
-       (4, 'Aston Martin', 'Vantage V12', 'LU3232', 400000, 3),
-       (5, 'Nissan', 'Fairlady Z', 'WA8922', 90000, 3),
-       (6, 'Toyota', 'Supra', 'WF6546', 130000, 3),
-       (7, 'Ariel', 'Atom', 'WPR4322', 400000, 3);
+       (2, 'Vespa', '254z', 'WXB132', 10000, 2),
+       (3, 'Ducati', '500R', 'LU5532', 120000, 3),
+       (4, 'Aston Martin', 'Vantage V12', 'LU3232', 400000, 1),
+       (5, 'Opel', 'Movano', 'WA8922', 90000, 4),
+       (6, 'Volvo', 'Iron Knight', 'WF6546', 500000, 5),
+       (7, 'Ariel', 'Atom', 'WPR4322', 400000, 6);
